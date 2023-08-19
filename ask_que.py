d03084417df03403402141147e5996d407b7b8ab
Sagar Patidar <sagarpatidar2002@gmail.com>
2023-08-19 15:33:08 +0530

    Add 50:50 lifeline

--- ask_que.py
+++ ask_que.py
@@ -1,22 +1,34 @@
-def ask_que(que, price_list, i):
+import fifty_fifty
+
+
+def ask_que(que, price_list, i, columns, lifeline, used_lifeline):
+
     print()
     print()
-    print("KBC".center(175))
-    print(f"Current price : {price_list[i]}                  After this question : {price_list[i+1]}".center(175))
-    print("-------------------------------------------------------------".center(175))
-    print(("" + que[0]).center(175))
+    print("KBC".center(columns))
+    print("_______________________________________________________________".center(columns))
+    print(f"Current price : {price_list[i]}                  After this question : {price_list[i+1]}".center(columns))
+    print("_______________________________________________________________".center(columns))
+    print(f"Remaining Life line :- {lifeline}".center(columns))
+    print("_______________________________________________________________".center(columns))
+    print(("" + que[0]).center(columns))
     print()
 
     options = que[1].split(",")
 
-    print(f"{options[0]}                  {options[1]}".center(175))
-    print(f"{options[2]}                  {options[3]}".center(175))
+    print(f"{options[0]}                  {options[1]}".center(columns))
+    print(f"{options[2]}                  {options[3]}".center(columns))
     print()
-    print("Select an Option : ".center(160))
-    ans = input("".center(90)).upper()
-
-    if ans == que[2]:
-        return True
-    else:
-        return False
-
+    print("Select an Option : ".center(columns-15))
+    ans = input("".center(70)).upper()
+    while True:
+        if ans == que[2]:
+            return True
+        elif ans in used_lifeline:
+            print("You already used this lifeline try a different one".center(columns))
+            print("Select a different Option : ".center(columns - 15))
+            ans = input("".center(70)).upper()
+        elif ans == "H":
+            ans = fifty_fifty.fifty(que, price_list, i, columns, lifeline, used_lifeline)
+        else:
+            return False
--- main.py
+++ main.py
@@ -1,43 +1,60 @@
 import que
 import time
 from ask_que import ask_que
+import shutil
+    
 
 questions = que.read_que()
 
 prize_list = [
     "₹0",
-    "₹100",
-    "₹200",
-    "₹300",
-    "₹500",
     "₹1000",
     "₹2000",
-    "₹4000",
-    "₹8000",
-    "₹16000",
-    "₹32000",
-    "₹64000",
-    "₹125000",
-    "₹250000",
-    "₹500000",
-    "₹1000000"
+    "₹5000",
+    "₹10000",
+    "₹20000",
+    "₹40000",
+    "₹80000",
+    "₹160000",
+    "₹320000",
+    "₹640000",
+    "₹1250000",
+    "₹2500000",
+    "₹5000000",
+    "₹10000000",
+    "₹70000000"
 ]
 
-for i in range(14):
+# Get windows size
+terminal_size = shutil.get_terminal_size()
+rows, columns = terminal_size.lines, terminal_size.columns
+
+columns += 50
+
+
+lifeline = ["50:50(H)", "Audience Poll(P)", "Switch the Question(Q)", "Double Dip(U)"]
+used_lifeline = []
+
+for i in range(15):
 
     question = [questions["Questions"][i], questions['Options'][i], questions['Answers'][i]]
     print()
-    if ask_que(question, prize_list, i):
-        print("Correct Answer. Next Question in 5 second".center(175))
+
+    if ask_que(question, prize_list, i, columns, lifeline, used_lifeline):
+        if i == 14:
+            print("Correct Answer.".center(columns))
+            print(f"Congratulations! You won {prize_list[i]}".center(columns))
+            break
+        print("Correct Answer. Next Question in 5 second".center(columns))
         time.sleep(5)
 
     else:
-        print("Incorrect Answer!".center(175))
-        print(f"Correct Answer is option {question[2]}".center(175))
+        print("Incorrect Answer!".center(columns))
+        print(f"Correct Answer is option {question[2]}".center(columns))
         if i == 0:
-            print("Unfortunately, you didn't win any prize. Better luck next time!".center(175))
+            print("Unfortunately, you didn't win any prize. Better luck next time!".center(columns))
             break
-        print(f"Congratulations! You won {prize_list[i]}".center(175))
+        print(f"Congratulations! You won {prize_list[i]}".center(columns))
         break
 
 
